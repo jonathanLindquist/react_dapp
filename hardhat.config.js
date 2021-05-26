d6cb08c820b57c0384b8fbd93ea41619e76f2119
Jonathan Lindquist <jonathan.lindquist17@gmail.com>
2021-05-26 17:50:49 -0500

    update src location

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,11 +1,11 @@
 export const solidity = "0.8.3";
 
 export const paths = {
-  artifacts: '.src.artifacts',
+	artifacts: "./src/artifacts",
 };
 
 export const networks = {
-  hardhat: {
-    chainId: 1337
-  }
+	hardhat: {
+		chainId: 1337,
+	},
 };
